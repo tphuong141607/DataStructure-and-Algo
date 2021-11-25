2d7e1f0d6e817a652a0fb52b8ee3fd22f97a8332
tphuong141607 <tphuong141607@gmail.com>
2021-11-25 14:44:48 -0500

    added some comments

--- Data Structure/queue/queue.js
+++ Data Structure/queue/queue.js
@@ -1,26 +1,30 @@
 "use strict";
 
 import DoubleLinkedList from "../linked_list/double_ll.js";
-
+/** Queue is implemented using Double Linked List */
 class Queue {
   constructor() {
     this.items = new DoubleLinkedList();
   }
 
+  // Add to head
   enqueue(item) {
     this.items.insertAtBeginning(item);
   }
 
+  // Remove tail
   dequeue() {
     const tail = this.items.tail;
     const removed = this.items.removeAt(this.items.length - 1);
     return removed === -1 ? -1 : tail.data;
   }
 
+  // Check if the queue is empty
   isEmpty() {
     return this.items.length === 0;
   }
 
+  // get the queue's length
   size() {
     return this.items.length;
   }
